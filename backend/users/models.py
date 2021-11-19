1e712e338abd1a13262ca6e12b811c9e11fbc073
Crowdbotics <team@crowdbotics.com>
2021-11-19 16:15:09 +0000

    Update models

--- backend/users/models.py
+++ backend/users/models.py
@@ -28,6 +28,11 @@ class User(AbstractUser):
         blank=True,
         max_length=254,
     )
+    sex = models.CharField(
+        max_length=2,
+        null=True,
+        blank=True,
+    )
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
